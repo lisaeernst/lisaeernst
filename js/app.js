3d00ef18ac83191f365d1368fa62516212917064
lisaeernst <78939274+lisaeernst@users.noreply.github.com>
2021-08-30 20:54:59 -0700

    added images

---
+++ js/app.js
@@ -0,0 +1,8 @@
+/* LISA ERNST FRONT END WEB DEVELOPER */
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+    myInput.focus()
+})
