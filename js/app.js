e8147ac50a492ebf5b2624f0c16c49533e46e1c2
lisaeernst <78939274+lisaeernst@users.noreply.github.com>
2021-09-06 11:22:24 -0700

    adding back to top button

--- js/app.js
+++ js/app.js
@@ -6,27 +6,21 @@ baguetteBox.run('.project-writeup');
 /* back to top button */
 
 //Get the button
-let mybutton = document.getElementById("btn-back-to-top");
+var mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-    scrollFunction();
-};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-    if (
-        document.body.scrollTop > 10 ||
-        document.documentElement.scrollTop > 10
-    ) {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         mybutton.style.display = "block";
     } else {
         mybutton.style.display = "none";
     }
 }
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
 
-function backToTop() {
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
