f2221a84fe129c11084c367d633f2ae8b327ca3c
lisaeernst <78939274+lisaeernst@users.noreply.github.com>
2021-09-06 11:50:02 -0700

    btt button

--- js/app.js
+++ js/app.js
@@ -5,21 +5,27 @@ baguetteBox.run('.project-writeup');
 
 /* back to top button */
 //Get the button
-var mybutton = document.getElementById("myBtn");
+let mybutton = document.getElementById("btn-back-to-top");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
+window.onscroll = function () {
+    scrollFunction();
+};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
         mybutton.style.display = "block";
     } else {
         mybutton.style.display = "none";
     }
 }
-
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
